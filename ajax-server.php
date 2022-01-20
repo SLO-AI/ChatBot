ae74c35b332628aef24793809b4b41f50df72c02
DanielsWrath <danielhaitink@gmail.com>
2022-01-20 23:30:15 +0100

    reformatted code and removed js from html

--- ajax-server.php
+++ ajax-server.php
@@ -1,11 +1,11 @@
 <?php
 $chat_dir = "chat";
-if(!file_exists($chat_dir)) mkdir($chat_dir);
+if (!file_exists($chat_dir)) mkdir($chat_dir);
 
 $default = "default-chat.log";
-if(!file_exists("$chat_dir/$default")) file_put_contents("$chat_dir/$default", "");
+if (!file_exists("$chat_dir/$default")) file_put_contents("$chat_dir/$default", "");
 
-switch($_POST["action"]) {
+switch ($_POST["action"]) {
     case "setcorpus":
         $filename = "tmp/" . $_POST["filename"];
         $corpus = $_POST["corpus"];
@@ -18,15 +18,15 @@ switch($_POST["action"]) {
         writeChat($chatfile);
         echo chatLog($chatfile);
         break;
-    
+
     case "poll":
         $chatfile = "$chat_dir/" . $_POST["room"] . ".log";
         echo chatLog($chatfile);
         break;
-        
+
     case "room":
         echo getRoom($chat_dir);
-        break;     
+        break;
 
     case "clear":
         $chatfile = "$chat_dir/" . $_POST["room"] . ".log";
@@ -42,29 +42,33 @@ switch($_POST["action"]) {
         break;
 }
 
-function writeChat($f) {
+function writeChat($f)
+{
     $format = "<p>%s&nbsp;<span class='namelite'>%s</span>&nbsp;%s</p>";
     $str = sprintf($format, date("H:i"), $_POST["name"], $_POST["msg"]);
     file_put_contents($f, "$str\n", FILE_APPEND | LOCK_EX);
 }
 
-function chatLog($f) {
-    if(file_exists($f)) $log = file_get_contents($f); else $log = "";
+function chatLog($f)
+{
+    if (file_exists($f)) $log = file_get_contents($f); else $log = "";
     return $log;
 }
 
-function getRoom($dir) {    
+function getRoom($dir)
+{
     $ffs = preg_grep('/^([^.])/', scandir($dir));
     $ffs = array_values($ffs);
-    foreach($ffs as $key=>$ff) {
+    foreach ($ffs as $key => $ff) {
         $ffs[$key] = explode(".", $ff)[0];
     }
     //$new = $_POST['new'];
-    if(isset($_POST['new'])) {
+    if (isset($_POST['new'])) {
         $new = $_POST['new'];
         file_put_contents("$dir/$new" . ".log", "");
-        $ffs = array_merge([ $new ], $ffs);
+        $ffs = array_merge([$new], $ffs);
     }
-    return json_encode($ffs);    
+    return json_encode($ffs);
 }
+
 ?>
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const nlp = require('@nlpjs/nlp');
 const langnl = require('@nlpjs/lang-nl');
 const requestrn = require('@nlpjs/request-rn');
 
-window.nlpjs = { ...core, ...nlp, ...langnl, ...requestrn };
+window.nlpjs = {...core, ...nlp, ...langnl, ...requestrn};
