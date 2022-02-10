2a21aeb84fa2d305e7dbcf4cbd48432e5e5c02a7
DanielsWrath <danielhaitink@gmail.com>
2022-02-10 15:59:33 +0100

    added download button

--- js/client.js
+++ js/client.js
@@ -2,9 +2,17 @@ const load = function() {
     const chatroom = new ChatRoom();
     const bot = new Bot();
     const msg = document.getElementById("chat-input");
+    const downloadButton = document.getElementById("download-button");
+    const enter = document.getElementById("chat-enter");
+    const fileDrop = document.getElementById("file-drop");
 
-    const loadCorpus = (file) => {
+    let currentCorpus = null;
+    let corpusName = null;
+
+    const loadCorpus = (file, fileName) => {
         console.log("Set corpus");
+        currentCorpus = file;
+        corpusName = fileName;
 
         bot.setCorpus(file).then(() => {
             console.log("training corpus");
@@ -21,6 +29,7 @@ const load = function() {
         if (msg.value === null || msg.value.length <= 0) {
             return;
         }
+
         const content = msg.value;
 
         chatroom.addMessage(content);
@@ -29,36 +38,55 @@ const load = function() {
         bot.getReply(content).then(r => {
             chatroom.addReply(r.answer);
         })
-
     };
+
     msg.addEventListener("keypress", (e) => {
         if (e.key === "Enter") {
             sendMessage();
         }
     });
 
-    const enter = document.getElementById("chat-enter");
+    function downloadObjectAsJson(){
+        if (!currentCorpus)
+            return;
+
+        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(currentCorpus, null, 2));
+        const anchor = document.createElement('a');
+
+        anchor.setAttribute("href", dataStr);
+        anchor.setAttribute("download", corpusName);
+
+        document.body.appendChild(anchor); // required for firefox
+
+        anchor.click();
+        anchor.remove();
+    }
+
+    downloadButton.addEventListener("click", () => {
+        downloadObjectAsJson()
+    });
+
     enter.addEventListener("click", () => {
         sendMessage();
     });
 
-    fetch("./chatdata/bol.json").then((r) => {
-        r.json().then((r) => {
-            loadCorpus(r);
-        });
-
-    });
-
-    new DropArea(document.getElementById("file-drop"), (files) => {
+    new DropArea(fileDrop, (files) => {
         console.log(files);
         const fr = new FileReader()
 
         fr.onload = function(){
             const r = JSON.parse(fr.result)
-            loadCorpus(r);
+            loadCorpus(r, fr.name);
         }
 
         fr.readAsText(files[0]);
+
+    });
+
+    fetch("./chatdata/bol.json").then((r) => {
+        r.json().then((r) => {
+            loadCorpus(r, "bol.json");
+        });
 
     });
 };
